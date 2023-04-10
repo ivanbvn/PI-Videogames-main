394bb16210620d32595cd6faff2d0ae39976182d
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2023-04-10 13:19:54 -0300

    fixes

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const getAllGenres = require('./src/controllers/getAllGenres.js');
 const { conn } = require('./src/db.js');
-const { PORT } = process.env
+const { PORT } = process.env || 3001
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
--- api/src/db.js
+++ api/src/db.js
@@ -3,9 +3,13 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -34,8 +38,8 @@ const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Videogame.belongsToMany(Genre, {through: 'videogameGenre'})
-Genre.belongsToMany(Videogame, {through: 'videogameGenre'})
+Videogame.belongsToMany(Genre, { through: 'videogameGenre' })
+Genre.belongsToMany(Videogame, { through: 'videogameGenre' })
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
